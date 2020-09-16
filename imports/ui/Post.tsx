cc7ece3588f445d351bf42d546c73fcb4397ae9d
Ion Harea <ion.harea@ffi.utm.md>
2020-09-17 00:34:23 +0300

    Fetched data from Reddit

--- imports/ui/Post.tsx
+++ imports/ui/Post.tsx
@@ -1,27 +1,58 @@
 import React from 'react';
-
 import axios from 'axios';
+
+import { PostData, PostsCollection } from '../api/posts'
 
 
 export class Post extends React.Component {
 
-    posts: Array<Object> = [];
+    state = {
+        posts: []
+    }
 
     componentDidMount() {
         const link = `https://www.reddit.com/r/popular.json`;
         axios.get(link)
             .then(res => {
-                res.data.data.children.forEach((child: Object) => this.posts.push(child));
-            }).catch(error => {
-                console.error(`There was an error fetching data from ${link}!`, error);
+                res.data.data.children.map((child: Object) => {
+                    const newObject: PostData = {
+                        _id: child.data.id,
+                        name: child.data.name,
+                        title: child.data.title,
+
+                        url: child.data.url,
+                        createdAt: child.data.created
+                    }
+                    this.setState(prevState => prevState.posts.push(newObject));
+
+                });
+                return this.state.posts
+                
+            // }).then(posts => {
+            //     posts.forEach((post: PostData) => {
+            //         PostsCollection.insert(post)
+            //     })
+            })
+            .catch(error => {
+                console.error(`There was an error fetching data from ${link}! or it happened while inserting in database!`, error);
             });
     }
 
     render() {
         return (
-            <ul>
-                {this.posts.map(post => <li> {post.data.author} </li>)}
-            </ul>
+            <div>
+                <br />
+                { this.state.posts.map((post: PostData, index) =>
+                    <ul>
+                        <h3>Post number {index + 1}</h3>
+                        <li>ID: {post._id}</li>
+                        <li>NAME: {post.name}</li>
+                        <li>TITLE: {post.title}</li>
+                        <li>URL: {post.url}</li>
+                        <li>CREATEDAT: {post.createdAt}</li>
+                        <br />
+                    </ul>)}
+            </div>
         )
     }
 }
--- server/main.ts
+++ server/main.ts
@@ -5,28 +5,28 @@ function insertLink(title: string, url: string) {
   LinksCollection.insert({ title, url, createdAt: new Date() });
 }
 
-Meteor.startup(() => {
+  Meteor.startup(() => {
 
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink(
-      'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
-    );
+    // If the Links collection is empty, add some data.
+    if (LinksCollection.find().count() === 0) {
+      insertLink(
+        'Do the Tutorial',
+        'https://www.meteor.com/tutorials/react/creating-an-app'
+      );
 
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
+      insertLink(
+        'Follow the Guide',
+        'http://guide.meteor.com'
+      );
 
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
+      insertLink(
+        'Read the Docs',
+        'https://docs.meteor.com'
+      );
 
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
-  }
-});
+      insertLink(
+        'Discussions',
+        'https://forums.meteor.com'
+      );
+    }
+  });
